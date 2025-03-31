35a252dcbe2d4c9bad751ff1e3dff58ff864a073
LehaoHuang <a1896897@adelaide.edu.au>
2025-03-31 14:10:21 +1030

    create a file which use to store the cache file.

--- Proxy.py
+++ Proxy.py
@@ -172,10 +172,13 @@ while True:
 
       # Get the response from the origin server
       # ~~~~ INSERT CODE ~~~~
+      Buffsize = BUFFER_SIZE
+      originServerResponse = originServerSocket.recv(Buffsize)
       # ~~~~ END CODE INSERT ~~~~
 
       # Send the response to the client
       # ~~~~ INSERT CODE ~~~~
+      clientSocket.sendall(originServerResponse)
       # ~~~~ END CODE INSERT ~~~~
 
       # Create a new file in the cache for the requested file.
@@ -187,6 +190,7 @@ while True:
 
       # Save origin server response in the cache file
       # ~~~~ INSERT CODE ~~~~
+      cacheFile.write(originServerResponse)
       # ~~~~ END CODE INSERT ~~~~
       cacheFile.close()
       print ('cache file closed')
